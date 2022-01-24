ac61d7ee7a1187c07cbf9902ad3895a33438e02b
julesssal <91210627+julesssal@users.noreply.github.com>
2022-01-24 12:54:11 -0500

    myInfo.py

---
+++ myInfo.py
@@ -0,0 +1,7 @@
+#CS175L-01
+#Julia Salerno
+#myInfo
+print("My name is Julia Salerno")
+print("I live on Westgate Drive in Sparta NJ, 07871")
+print("My phone number is 973-903-9739")
+print("I am a Computer Science Major")
